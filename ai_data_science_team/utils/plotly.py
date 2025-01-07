4343b2f6178c0e07224009c5229f99306fd16752
Matt Dancho <mdancho@gmail.com>
2025-01-07 08:45:13 -0500

    plotly_from_dict() util function

---
+++ ai_data_science_team/utils/plotly.py
@@ -0,0 +1,21 @@
+
+
+import json
+import plotly.io as pio
+
+def plotly_from_dict(plotly_graph_dict: dict):
+    """
+    Convert a Plotly graph dictionary to a Plotly graph object.
+    
+    Parameters:
+    -----------
+    plotly_graph_dict: dict
+        A Plotly graph dictionary.
+        
+    Returns:
+    --------
+    plotly_graph: plotly.graph_objs.graph_objs.Figure
+        A Plotly graph object.
+    """
+    
+    return pio.from_json(json.dumps(plotly_graph_dict))
